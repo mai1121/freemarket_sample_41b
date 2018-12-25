12e86df678853d07d68044066d0ada02eee9615f
玉理周平 <tamaritamari@tamarishuuheinoMacBook-Pro.local>
2018-12-25 16:32:52 +0900

    Itemモデルの作成
    外部キー関連のものは一旦コメントアウトしている。

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20181225071850) do
+
+  create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string  "name",                              null: false
+    t.text    "description",         limit: 65535
+    t.integer "size"
+    t.integer "status"
+    t.integer "delivery_fee_method"
+    t.integer "delivery_method"
+    t.integer "ships_from"
+    t.integer "days_to_ship"
+    t.integer "price",                             null: false
+  end
 
 end
