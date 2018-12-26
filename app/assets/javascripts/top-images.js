378f06c6f38969395c43403f09f3249ce46220e5
mai1121 <maiukawa@maiukawanoMacBook-ea.local>
2018-12-26 12:12:43 +0900

    Edit top image slick

--- app/assets/javascripts/top-images.js
+++ app/assets/javascripts/top-images.js
@@ -1,14 +1,10 @@
 $(document).on('turbolinks:load', function() {
 	$(function(){
-		// ドットにカーソル当てた時の処理と外した時の処置
-		$('.top-images__dot').hover(
-			function(){
-			$(this).addClass('dot-active');
-		},
-			function(){
-			$(this).removeClass('dot-active');
-		}
-		);
+		$('.top-images__area').slick({
+      autoplay: true,
+      autoplaySpeed: 1000,
+      dots: true,
 		});
+	});
  });
 
