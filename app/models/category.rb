4c5e270011fc6386a7ffa02218ce2d8f7830a242
玉理周平 <tamaritamari@tamarishuuheinoMacBook-Pro.local>
2018-12-26 20:35:32 +0900

    Update category.rb
    categoryのモデルにメソッドを追加
    　・親要素（最上位要素）のリストを取得する
    　・引数の子要素のリストを取得する

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,13 +1,14 @@
 class Category < ApplicationRecord
   has_many :items
 
-  @@path_length_of_parent = 1
+  @@path_length_of_parents = 1
+  @@path_length_to_childs = 2
 
   def self.get_parents_list
-    return Category.where('length(path_to_root) = ?', @@path_length_of_parent)
+    return Category.where('length(path_to_root) = ?', @@path_length_of_parents)
   end
 
   def self.get_childs(parent_path)
-    return Category.where('path_to_root like ?', "#{parent_path}%").where('length(path_to_root) = ?', parent_path.to_s.length + 2)
+    return Category.where('path_to_root like ?', "#{parent_path}%").where('length(path_to_root) = ?', parent_path.to_s.length + @@path_length_to_childs)
   end
 end
