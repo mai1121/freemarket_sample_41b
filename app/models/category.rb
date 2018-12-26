94f8f5002b5b24bbe7faebfd58e29cc7bc97d17e
玉理周平 <tamaritamari@tamarishuuheinoMacBook-Pro.local>
2018-12-26 20:32:39 +0900

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,13 @@
 class Category < ApplicationRecord
   has_many :items
+
+  @@path_length_of_parent = 1
+
+  def self.get_parents_list
+    return Category.where('length(path_to_root) = ?', @@path_length_of_parent)
+  end
+
+  def self.get_childs(parent_path)
+    return Category.where('path_to_root like ?', "#{parent_path}%").where('length(path_to_root) = ?', parent_path.to_s.length + 2)
+  end
 end
