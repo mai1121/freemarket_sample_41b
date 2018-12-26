b4117ba8ca24b37d4254b5b2d4fd94fb9b226423
玉理周平 <tamaritamari@tamarishuuheinoMacBook-Pro.local>
2018-12-26 17:39:41 +0900

    category migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181225073431) do
+ActiveRecord::Schema.define(version: 20181226083059) do
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",         null: false
+    t.string   "path_to_root"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "item_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "item_id",    null: false
