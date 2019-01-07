a4f18ead13382797e41ac19efaf80711c9f1f681
玉理周平 <tamaritamari@tamarishuuheinoMacBook-Pro.local>
2019-01-07 16:08:03 +0900

    login top ~ registaration top
    ルーティングの改造

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
+  get 'users/sign_up_top' => 'users#top'
   get '/signup/registration' => 'temp#registration'
-  get '/signup' => 'temp#index'
 end
