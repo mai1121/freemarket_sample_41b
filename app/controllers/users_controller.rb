a635df64137c90c2bf727a340cd730aeac94c095
玉理周平 <tamaritamari@tamarishuuheinoMacBook-Pro.local>
2019-01-09 14:42:59 +0900

    mypage 用のルーティング　＋ コントローラー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ class UsersController < ApplicationController
 
   def registration
   end
+
+  def show
+    binding.pry
+    @user = current_user if user_signed_in?
+    unless @user
+      redirect_to root_path, notice: "not signed in!!"
+    end
+  end
 end
