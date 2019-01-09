e837277ed4ab3eb09d6b387f9d9f48b7ebd03d4f
玉理周平 <tamaritamari@tamarishuuheinoMacBook-Pro.local>
2019-01-09 14:56:31 +0900

    ログインが死んでたのでfix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    binding.pry
     @user = current_user if user_signed_in?
     unless @user
       redirect_to root_path, notice: "not signed in!!"
