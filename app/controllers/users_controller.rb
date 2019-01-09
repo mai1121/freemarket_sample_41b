b894c34ecbe27ca56d9be99660e6d87b2d9340b1
玉理周平 <tamaritamari@tamarishuuheinoMacBook-Pro.local>
2019-01-09 15:22:29 +0900

    Update users_controller.rb
    ヘッダー用にカテゴリーとブランドを取得

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,9 @@ class UsersController < ApplicationController
     @user = current_user if user_signed_in?
     unless @user
       redirect_to root_path, notice: "not signed in!!"
+    else
+      @parent_categories = Category.roots()
+      @blands = ['シャネル','ナイキ','ルイ　ヴィトン','シュプリーム','アディダス','ブランド一覧']
     end
   end
 end
