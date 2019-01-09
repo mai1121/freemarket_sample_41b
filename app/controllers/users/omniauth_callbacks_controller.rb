c1887a3b4ed3f430038eee61524ee36cc6fe25d3
mai1121 <maiukawa@maiukawanoMacBook-ea.local>
2019-01-09 18:34:36 +0900

    Edit sns signi_n & sign_up view

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.find_for_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      flash[:notice] = "成功しました"
       sign_in_and_redirect @user, event: :authentication
     else
       render :template => "devise/registrations/new"
@@ -13,7 +12,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     @user = User.find_for_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      flash[:notice] = "成功しました"
       sign_in_and_redirect @user, event: :authentication
     else
       render :template => "devise/registrations/new"
