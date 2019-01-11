cf967263edf6e3c1fb9d123233605435d5d60af4
玉理周平 <tamaritamari@tamarishuuheinoMacBook-Pro.local>
2019-01-11 12:52:51 +0900

    Update users_controller.rb
    modify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   layout "mypage_layout", only: [:show, :identification, :logout, :card]
-  before_action :set_layout, only:[:show, :identification]
+  before_action :set_layout, only:[:show, :identification, :card]
 
   def top
   end
@@ -10,29 +10,21 @@ class UsersController < ApplicationController
   end
 
   def identification
-
   end
 
   def show
-
-
-  end
-
-  def set_layout
-    @user = current_user
-
-    redirect_to root_path, notice: "not signed in!!" unless @user
   end
 
   def logout
-
   end
 
   def card
-    @user = current_user
   end
 
-  def card
-    @user = current_user
-  end
+  private
+    def set_layout
+      @user = current_user
+      redirect_to root_path, notice: "not signed in!!" unless @user
+    end
+
 end
