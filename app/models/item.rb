8d714c068bc7924c686d8f3a19c0c7a1cd879822
玉理周平 <tamaritamari@tamarishuuheinoMacBook-Pro.local>
2019-01-12 11:55:13 +0900

    Update item.rb
    Add association to item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,9 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :price, presence: true
+  belongs_to :buyer, class_name: "User"
+  belongs_to :saler, class_name: "User"
+  validates :saler_id, presence: true
 
   has_many :item_images
   validates :item_images, length: { minimum: 1 }
