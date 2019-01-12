ef28d70001fc67382be2e99301a828bb540a3256
玉理周平 <tamaritamari@tamarishuuheinoMacBook-Pro.local>
2019-01-12 14:14:24 +0900

    new, create

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   validates :saler_id, presence: true
 
   has_many :item_images
+  accepts_nested_attributes_for :item_images
   validates :item_images, length: { minimum: 1 }
 
   belongs_to :category
