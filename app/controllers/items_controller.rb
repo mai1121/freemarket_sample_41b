77fd8a6a13079f4e96fa1b1c9643d767ea6d9b03
玉理周平 <tamaritamari@tamarishuuheinoMacBook-Pro.local>
2019-01-12 14:14:24 +0900

    new, create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :require_login, only: [:new, :create]
+
   def index
     @items = Item.includes(:item_images).all
     @parent_categories = Category.roots()
@@ -6,7 +8,7 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    
+
     @item_images = @item.item_images
     @saler = @item.saler
     @category = @item.category
@@ -14,4 +16,26 @@ class ItemsController < ApplicationController
     @grandparent_category = @category.root
     @brand = @item.brand
   end
+
+  def new
+    @item = Item.new
+    @item.item_images.build
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+    def require_login
+      redirect_to new_user_session_url
+    end
+
+    def item_params
+      params.require(:item).permit( :name, :description,  :category_id, :size, :status, :delivery_fee_method, :delivery_method, :ships_from, :days_to_ship, :price, item_images_attributes: [:image,]).merge(saler_id: current_user.id).merge(brand_id: set_brand_id)
+    end
+
+    def set_brand_id
+      Brand.where('name = ?', params[:brand_id])
+    end
 end
