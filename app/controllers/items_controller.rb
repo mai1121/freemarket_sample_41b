f641f5809dfa2edc73f9725b496f3c119d0cda68
mai1121 <maiukawa@maiukawanoMacBook-ea.local>
2019-01-12 19:37:32 +0900

    Edit controller & view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,22 @@ class ItemsController < ApplicationController
     @items = Item.includes(:item_images).all
     @parent_categories = Category.roots()
   end
+
+  def show
+    # アイテム
+    @item = Item.find(params[:id])
+    # アイテム画像
+    @item_images = @item.item_images
+    # 出品者
+    @saler = @item.saler
+    ##saler_idカラムで取得可能
+    # カテゴリー
+    @category = @item.category
+    @parent_category = @category.parent
+    @grandparent_category = @category.root
+    ## カテゴリーidカラムで取得可能
+    # ブランド
+    @brand = @item.brand
+    ## ブランドidカラムで取得可能
+  end
 end
