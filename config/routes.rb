7b1c121e2bcec172c48ffcb729e60a1d85fb39a9
mai1121 <maiukawa@maiukawanoMacBook-ea.local>
2019-01-12 12:23:02 +0900

    Set routing

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   root 'items#index'
+  resources :items, only[:index, :show]
   get 'users/sign_up_top' => 'users#top'
   get '/signup/registration' => 'users#registration'
   get '/users/mypage/' => 'users#show'
