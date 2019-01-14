8a8d373387491639a7831d0efc09c60f289eadf7
玉理周平 <tamaritamari@tamarishuuheinoMacBook-Pro.local>
2019-01-14 15:11:48 +0900

    file分割

--- app/assets/javascripts/item-category.js
+++ app/assets/javascripts/item-category.js
@@ -1,7 +1,20 @@
+
 $(document).on('turbolinks:load', function() {
+  //カテゴリーのセレクトボックス
   $(document).on('change', '.select-category', function() {
       var template = $(`#child0fCategory${$(this).val()}`);
       $(this).nextAll().remove();
       $(this.parentElement).append(template.html());
   });
+
+  $('#item_price').on('keyup',function(){
+    var price = ($(this).val());
+    if (price >= 300){
+      $('#salesCommission').html( '¥' + price / 10);
+      $('#saleProfit').html( '¥' + price * 0.9);
+    }else{
+      $('#salesCommission').html('-');
+      $('#saleProfit').html('-');
+    }
+  })
 });
