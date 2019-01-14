2a72e87294569d5a5101f550761e718d65ea01d1
mai1121 <maiukawa@maiukawanoMacBook-ea.local>
2019-01-14 14:57:22 +0900

    Modify indent

--- app/assets/javascripts/item_detail.js
+++ app/assets/javascripts/item_detail.js
@@ -1,21 +1,21 @@
 $(document).on('turbolinks:load', function() {
    $(function(){
-$('.slider').slick({
-  infinite: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-  infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  asNavFor: '.slider',
-  dots: false,
-  centerMode: true,
-  focusOnSelect: true
-});
-});
+      $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+      });
+      $('.slider-nav').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.slider',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
+      });
+    });
  });
