0f8cbfa047e670298922e9b627ffd94e27c397c3
玉理周平 <tamaritamari@tamarishuuheinoMacBook-Pro.local>
2019-01-15 18:35:58 +0900

    Update items_controller.rb
    インデントを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,15 +27,15 @@ class ItemsController < ApplicationController
   end
 
   private
-    def require_login
-      redirect_to new_user_session_url unless user_signed_in?
-    end
+  def require_login
+    redirect_to new_user_session_url unless user_signed_in?
+  end
 
-    def item_params
-      params.require(:item).permit( :name, :description,  :category_id, :size, :status, :delivery_fee_method, :delivery_method, :ships_from, :days_to_ship, :price, item_images_attributes: [:image,]).merge(saler_id: current_user.id).merge(brand_id: set_brand_id)
-    end
+  def item_params
+    params.require(:item).permit( :name, :description,  :category_id, :size, :status, :delivery_fee_method, :delivery_method, :ships_from, :days_to_ship, :price, item_images_attributes: [:image,]).merge(saler_id: current_user.id).merge(brand_id: set_brand_id)
+  end
 
-    def set_brand_id
-      Brand.where('name = ?', params[:brand_name])
-    end
+  def set_brand_id
+    Brand.where('name = ?', params[:brand_name])
+  end
 end
