abd5161f7be8007534b696e11614037cc69998a3
玉理周平 <tamaritamari@tamarishuuheinoMacBook-Pro.local>
2019-01-15 19:31:55 +0900

    Update item_images.rb
    use file to item_image model

--- spec/factories/item_images.rb
+++ spec/factories/item_images.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :item_image do
-<<<<<<< HEAD
-    image                  {Faker::Internet.url()}
     image                  {File.open("#{Rails.root}/public/images/no_image.jpg")}
   end
 end
