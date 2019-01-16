77d10da2439b35727ea724d50fc799fc2c4b0bcb
玉理周平 <tamaritamari@tamarishuuheinoMacBook-Pro.local>
2019-01-16 15:15:42 +0900

    Update seeds.rb
    - user = User.find(1)
    + user = User.first()

--- db/seeds.rb
+++ db/seeds.rb
@@ -7801,7 +7801,7 @@ end
 user = User.create(id:1,email:"bbb@gmail.com",password:"00000000",password_confirmation:"00000000",first_name:"綾波",last_name:"れい",first_name_kana:"アヤナミ",last_name_kana:"レイ",nickname:"rei",phone_number:"09011111111",credit_card_num:"1111222233334444",credit_card_expiration_month:"11",credit_card_expiration_year:"32",credit_card_security_num:"111",profile:"こんにちは",icon_image:"aaaaa",postal_code:"1111111",prefecture:"北海道",city:"札幌市西区",address:"1-1-1",building_name:"aaaビル",birth_year:"1992",birth_month:"11",birth_day:"21",uid:"00000000000",provider:"facebook"
 )
 
-user = User.find(1)
+user = User.first()
 
 item = Item.new(id:1, name: "魔法少女まどか☆マギカDVDボックス", description: "とても思い入れのある作品ですが、引っ越しに伴い手放すことにしました。保存状態良好です。", price: 15000, category_id: 581, saler_id: user.id)
 image = item.item_images.new(item_id: 1, image: File.open("#{Rails.root}/public/images/no_image.jpg"))
