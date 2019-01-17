4546da6e120e01d356d4ad7b878d0ef99d0391aa
mai1121 <maiukawa@maiukawanoMacBook-ea.local>
2019-01-17 21:34:05 +0900

    Modify helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def item_price(price)
-    item.price.to_s.gsub(/(\d)(?=(\d{3})+(?!\d))/, '\1,').insert(0,'¥ ')
+    price.to_s.gsub(/(\d)(?=(\d{3})+(?!\d))/, '\1,').insert(0,'¥ ')
   end
 end
