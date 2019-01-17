ba769e054d04db31337cd4bc0fcc6916506682c9
mai1121 <maiukawa@maiukawanoMacBook-ea.local>
2019-01-17 16:15:41 +0900

    Edit items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:purchase_top,:purchase]
+
   def index
     @items = Item.includes(:item_images).all
     @parent_categories = Category.roots()
@@ -14,4 +17,19 @@ class ItemsController < ApplicationController
     @grandparent_category = @category.root
     @brand = @item.brand
   end
+
+  def purchase_top
+    @item = Item.find(params[:id])
+    @item_image = @item.item_images.first
+  end
+
+  def purchase
+    @item = Item.find(params[:id])
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+
+    @purchase = Payjp::Charge.create(currency: 'jpy', amount: @item.price, card: params['payjpToken'] )
+    @item.update(buyer_id: current_user.id)
+    redirect_to root_path
+  end
+
 end
