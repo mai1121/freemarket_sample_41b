2f10b83c8d454d1fc20d8485d20ae649bec25a84
mai1121 <maiukawa@maiukawanoMacBook-ea.local>
2019-01-19 21:52:03 +0900

    Edit brand model & controller

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,2 +1,14 @@
 class BrandsController < ApplicationController
+  def index
+    @brand = Brand.all
+  end
+  def show
+    @brand = Brand.find_by(name: params[:id])
+    # binding.pry
+    @items = @brand.items
+
+    @categories = Brand.find_categories(@items)
+
+    # binding.pry
+  end
 end
--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,4 +2,16 @@ class Brand < ApplicationRecord
   has_many :items
 
   enum recommend_item: ['CHANEL','NIKE','LOUISVUITTON','Supreme','Adidas','brandslist']
+
+  def self.find_categories(items)
+    post_category = []
+    items.each do |item|
+      category = item.category
+      post_category << category
+    end
+    return post_category.uniq
+  end
+
+
+
 end
