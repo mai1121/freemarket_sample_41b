67b80156a46b15ed2a25803fb95a610b83148836
mai1121 <maiukawa@maiukawanoMacBook-ea.local>
2019-01-19 13:06:03 +0900

    Edit category controller & model

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,30 +5,12 @@ class CategoriesController < ApplicationController
   def show
     # 選択したカテゴリー
     @category = Category.find(params[:id])
-    @child_categories = @category.children
-    # 選択したカテゴリーの子孫要素を取得して変数に代。配列様式で格納されている。
+    # 上部に表示する子カテゴリー
+    @child_categories = @category.children.take(9)
+    # 選択したカテゴリーの子孫要素
     @descendants_categories = @category.descendants
-    
-    # idが大きい順（新しい順）にitemを表示
-    @items = items(@category,@descendants_categories).sort {|a, b| b <=> a }
+    # ブラウザに表示するitem
+    @items = Category.items(@category,@descendants_categories).sort {|a, b| b <=> a }
 
-    # binding.pry
-  
-    
-  end
-
-  def items(selected_category,descendants_categories)
-    post_items = []
-    # 選択したカテゴリーの持つitemを配列形式で取得
-    items = selected_category.items
-    # 配列に追加
-    post_items = post_items|items
-
-    descendants_categories.each do |descendants_category|
-      items = descendants_category.items
-      post_items = post_items|items
-    end
-
-    return post_items
   end
 end
