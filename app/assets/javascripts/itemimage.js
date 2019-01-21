b59215431ed106b762a4099f078a9d27b1dba7e9
玉理周平 <tamaritamari@tamarishuuheinoMacBook-Pro.local>
2019-01-21 16:06:27 +0900

    画像の編集削除機能の実装

--- app/assets/javascripts/itemimage.js
+++ app/assets/javascripts/itemimage.js
@@ -1,72 +1,98 @@
 $(document).on('turbolinks:load', function() {
 
-  $(document).on('click','.sell-upload-delete',function(){
+  $(document).on('click', '.sell-upload-delete', function() {
     $(this).closest('li');
     var id = $(this).closest('li').attr('id');
-    $('#destroyAttrOf' + id.substr(id.indexOf('_')+1)).val(true);
+    $('#destroyAttrOf' + id.substr(id.indexOf('_') + 1)).val(true);
     $(this).closest('li').remove();
+    var ul = $('.sell-upload-items ul');
+    changeUploadImagesClass(ul)
+  })
+
+  $(document).on('click', '.sell-upload-edit', function() {
+    var listElement = $(this).closest('li');
+    var id = listElement.attr('id');
+    var fileElement = $('#imageAttrOf' + id.substr(id.indexOf('_') + 1));
+    fileElement.click();
   })
 
   var $dropZone = $('#drop_zone');
   //ドラッグ時のドロップフィールド見た目
-  $dropZone.on('dragover',function(e){
+  $dropZone.on('dragover', function(e) {
     e.stopPropagation();
     e.preventDefault();
-    $dropZone.css({'box-shadow':'0px 0px 10px',
-     'border':'1px solid #000'});
+    $dropZone.css({
+      'box-shadow': '0px 0px 10px',
+      'border': '1px solid #000'
+    });
   });
 
-  $dropZone.on('dragleave',function(e){
+  $dropZone.on('dragleave', function(e) {
     e.stopPropagation();
     e.preventDefault();
-    $dropZone.css({'box-shadow':'none',
-     'border': '1px dashed #ccc'})
-  });
-
-
-  $("input[type=file]").on('change',function(){
-    var files = this.files;
-    for (var i = 0; i < files.length; i++) {
-      var fr = new FileReader();
-        fr.onload = function(fr) {
-            var ul = $('.sell-upload-items ul');
-            ul.append(buildListItem(this.result));
-            changeUploadImagesClass(ul)
-        };
-      fr.readAsDataURL(files[i]);
-    }
-  })
-
-  $dropZone.on("click", function() {
-    $("#item_image").click();
+    $dropZone.css({
+      'box-shadow': 'none',
+      'border': '1px dashed #ccc'
+    })
   });
 
 
   var dropZone = document.getElementById("drop_zone");
-  if(dropZone){
+  if (dropZone) {
     dropZone.addEventListener("drop", function(e) {
       e.stopPropagation();
       e.preventDefault();
 
       var files = e.dataTransfer.files;
-      addUploadImages(files)
       var formData = document.querySelector('.sell-form')
-      行けたらいいな
       image_container.appendChild(files)
     }, false);
   };
+
+
+  $dropZone.on("click", function() {
+    $("#item_image").click();
+  });
+
+  $("input[type=file]#item_image").on('change', function() {
+    var files = this.files;
+    for (var i = 0; i < files.length; i++) {
+      var fr = new FileReader();
+      fr.onload = function(fr) {
+        var ul = $('.sell-upload-items ul');
+        ul.append(buildListItem(this.result));
+        changeUploadImagesClass(ul)
+      };
+      fr.readAsDataURL(files[i]);
+    }
+  })
+
+  $(".sell-single-image-file").on('change', function() {
+    var files = this.files;
+    var id = $(this).attr('id').replace('imageAttrOf', '')
+    for (var i = 0; i < files.length; i++) {
+      var fr = new FileReader();
+      fr.onload = function(fr) {
+        var item_img = $('#image_' + id + " img");
+        item_img.attr('src', this.result);
+        $('#cacheAttrOf' + id).remove();
+      };
+      fr.readAsDataURL(files[i]);
+    }
+  })
+
 });
 
-function deleteItemImage(i){
+function deleteItemImage(i) {
   $('li.sell-upload-item')[i].remove();
   changeUploadImagesClass($('.sell-upload-items ul'));
 }
 
 
-function buildListItem(src){
+function buildListItem(src) {
   var image = `<img src="${src}" alt="" class="">`;
   var html =
-          `<li class="sell-upload-item">
+    `<li class="sell-upload-item">
             <figure class="sell-upload-figure landscape">
               ${image}
             </figure>
@@ -79,26 +105,10 @@ function buildListItem(src){
 }
 
 
-function changeUploadImagesClass(ul){
+function changeUploadImagesClass(ul) {
   $imagesBox = $('.sell-upload-items');
   boxclass = `sell-upload-items have-item${ul.children('li').length}`;
-  $imagesBox.removeAttr().attr({'class': boxclass});
-}
-
-function addUploadImages(files){
-
-  var formData = new FormData()
-  for (var i = 0; i < files.length; i++) {
-    (function() {
-      var fr = new FileReader();
-      fr.onload = function() {
-        var ul = $('.sell-upload-items ul');
-        ul.append(buildListItem(fr.result));
-        changeUploadImagesClass(ul)
-      };
-      fr.readAsDataURL(files[i]);
-    })();
-  formData.append("item[item_images][image][]", files[i]);
-  }
-  $('#drop_zone').css({'box-shadow':'none','border': '1px dashed #ccc'});
+  $imagesBox.removeAttr().attr({
+    'class': boxclass
+  });
 }
